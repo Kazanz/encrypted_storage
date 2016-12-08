ec44be43fab8168ddfd2d68ab5cee569942e8469
Zach Kazanski <kazanski.zachary@gmail.com>
2016-12-08 11:25:32 -0500

    fix(setup): remove wsgiref for python3.x compatability

--- setup.py
+++ setup.py
@@ -18,7 +18,6 @@ if __name__ == '__main__':
             'redis==2.10.3',
             'requests==2.7.0',
             'six==1.9.0',
-            'wsgiref==0.1.2',
         ],
         classifiers=[
             'Programming Language :: Python',
